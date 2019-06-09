51a8f97855feb2a67a860c3815131293fd7bf20d
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-09 09:01:31 +0800

    creating the route, added the config for multer

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+var multer = require('multer');
+var upload = multer({ dest: './uploads/' });
+
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.post('/', upload.single('myFile'), function (req, res) {
+    res.send(req.file);
 });
 
 app.listen(3000);
